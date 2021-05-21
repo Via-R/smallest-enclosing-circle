d49b9b03dc95c19dade96e7d3625e4927cb3a169
vrets <vadymr@zeuselectronics.eu>
2021-05-21 23:04:14 +0300

    Add random points generator

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from typing import List
+
 import algorithm
 import matplotlib.pyplot as plt
+import random
+from scipy.stats import truncnorm
+
+
+def get_truncated_normal(mean: float = 0, sd: float = 1, low: float = 0, upp: float = 10):
+    return truncnorm((low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd)
 
 
 def read_from_file():
@@ -23,8 +31,8 @@ def draw(points, circle):
     plt.figure(figsize=(6, 6))
 
     plt.plot(xs, ys, "og")
-    plt.xlim(-0.5, 1.5)
-    plt.ylim(-0.5, 1.5)
+    plt.xlim(-1.5, 1.5)
+    plt.ylim(-1.5, 1.5)
 
     circle_img = plt.Circle((circle[0], circle[1]), circle[2], color='r', fill=False)
     plt.gca().add_patch(circle_img)
@@ -35,8 +43,23 @@ def draw(points, circle):
     plt.show()
 
 
+def generate_input(points_amount: int = 88) -> List[List[float]]:
+    generator = get_truncated_normal(mean=0, sd=0.3, low=-1, upp=1)
+    return [[generator.rvs(), generator.rvs()] for _ in range(points_amount)]
+
+
 def main():
-    points = read_from_file()
+    # input_source = input(
+    #     "Please select input source (type 1 or 2):\n1) input.txt file\n2) random generation\nChoice: "
+    # ).lower()
+    # if input_source == "1":
+    #     points = read_from_file()
+    # elif input_source == "2":
+    #     points = generate_input()
+    # else:
+    #     print("Invalid choice, please try again")
+    #     return
+    points = generate_input()
     circle = algorithm.make_circle(points)
     draw(points, circle)
 
